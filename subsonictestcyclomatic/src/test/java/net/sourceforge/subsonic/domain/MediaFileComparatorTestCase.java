70d72420b73e0b69c298e5530595d85242114a04
noisyfox <timemanager.rick@gmail.com>
2017-06-07 12:43:58 +1000

    Add cyclomatic test cases

--- subsonictestcyclomatic/src/test/java/net/sourceforge/subsonic/domain/MediaFileComparatorTestCase.java
+++ subsonictestcyclomatic/src/test/java/net/sourceforge/subsonic/domain/MediaFileComparatorTestCase.java
@@ -1,124 +1,139 @@
-/*
- This file is part of Subsonic.
-
- Subsonic is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- Subsonic is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Subsonic.  If not, see <http://www.gnu.org/licenses/>.
-
- Copyright 2009 (C) Sindre Mehus
- */
 package net.sourceforge.subsonic.domain;
 
 import junit.framework.TestCase;
 
-/**
- * @author Sindre Mehus
- * @version $Id: MediaFileComparatorTestCase.java 3019 2012-06-20 17:45:16Z sindre_mehus $
- */
 public class MediaFileComparatorTestCase extends TestCase {
 
-	private final MediaFileComparator comparator = new MediaFileComparator(true);
+    private final MediaFileComparator comparator_1 = new MediaFileComparator(true);
 
-	public void testCompareAlbums() throws Exception {
+    private final MediaFileComparator comparator_2 = new MediaFileComparator(false);
 
-		MediaFile albumA2012 = new MediaFile();
-		albumA2012.setMediaType(MediaFile.MediaType.ALBUM);
-		albumA2012.setPath("a");
-		albumA2012.setYear(2012);
+    // execute path: 1-2-15
+    private boolean TC1() {
+        MediaFile album1 = new MediaFile();
+        album1.setMediaType(MediaFile.MediaType.MUSIC);
 
-		MediaFile albumB2012 = new MediaFile();
-		albumB2012.setMediaType(MediaFile.MediaType.ALBUM);
-		albumB2012.setPath("b");
-		albumB2012.setYear(2012);
+        MediaFile album2 = new MediaFile();
+        album2.setMediaType(MediaFile.MediaType.DIRECTORY);
 
-		MediaFile album2013 = new MediaFile();
-		album2013.setMediaType(MediaFile.MediaType.ALBUM);
-		album2013.setPath("c");
-		album2013.setYear(2013);
+        return comparator_1.compare(album1, album2) == 1;
+    }
 
-		MediaFile albumWithoutYear = new MediaFile();
-		albumWithoutYear.setMediaType(MediaFile.MediaType.ALBUM);
-		albumWithoutYear.setPath("c");
+    // execute path: 1-3-4-15
+    private boolean TC2() {
+        MediaFile album1 = new MediaFile();
+        album1.setMediaType(MediaFile.MediaType.DIRECTORY);
 
-		assertEquals(0, comparator.compare(albumWithoutYear, albumWithoutYear));
-		assertEquals(0, comparator.compare(albumA2012, albumA2012));
+        MediaFile album2 = new MediaFile();
+        album2.setMediaType(MediaFile.MediaType.MUSIC);
 
-		assertEquals(-1, comparator.compare(albumA2012, albumWithoutYear));
-		assertEquals(-1, comparator.compare(album2013, albumWithoutYear));
-		assertEquals(1, comparator.compare(album2013, albumA2012));
+        return comparator_1.compare(album1, album2) == -1;
+    }
 
-		assertEquals(1, comparator.compare(albumWithoutYear, albumA2012));
-		assertEquals(1, comparator.compare(albumWithoutYear, album2013));
-		assertEquals(-1, comparator.compare(albumA2012, album2013));
+    // execute path: 1-3-5-2-15
+    private boolean TC3() {
+        MediaFile album1 = new MediaFile();
+        album1.setMediaType(MediaFile.MediaType.ALBUM);
 
-		assertEquals(-1, comparator.compare(albumA2012, albumB2012));
-		assertEquals(1, comparator.compare(albumB2012, albumA2012));
-	}
+        MediaFile album2 = new MediaFile();
+        album2.setMediaType(MediaFile.MediaType.DIRECTORY);
 
-	public void testCompareDiscNumbers() throws Exception {
+        return comparator_1.compare(album1, album2) == 1;
+    }
 
-		MediaFile discXtrack1 = new MediaFile();
-		discXtrack1.setMediaType(MediaFile.MediaType.MUSIC);
-		discXtrack1.setPath("a");
-		discXtrack1.setTrackNumber(1);
+    // execute path: 1-3-5-6-4-15
+    private boolean TC4() {
+        MediaFile album1 = new MediaFile();
+        album1.setMediaType(MediaFile.MediaType.DIRECTORY);
 
-		MediaFile discXtrack2 = new MediaFile();
-		discXtrack2.setMediaType(MediaFile.MediaType.MUSIC);
-		discXtrack2.setPath("a");
-		discXtrack2.setTrackNumber(2);
+        MediaFile album2 = new MediaFile();
+        album2.setMediaType(MediaFile.MediaType.ALBUM);
 
-		MediaFile disc5track1 = new MediaFile();
-		disc5track1.setMediaType(MediaFile.MediaType.MUSIC);
-		disc5track1.setPath("a");
-		disc5track1.setDiscNumber(5);
-		disc5track1.setTrackNumber(1);
+        return comparator_1.compare(album1, album2) == -1;
+    }
 
-		MediaFile disc5track2 = new MediaFile();
-		disc5track2.setMediaType(MediaFile.MediaType.MUSIC);
-		disc5track2.setPath("a");
-		disc5track2.setDiscNumber(5);
-		disc5track2.setTrackNumber(2);
+    // execute path: 1-3-5-6-7-8-9-15
+    private boolean TC5() {
+        MediaFile album1 = new MediaFile();
+        album1.setMediaType(MediaFile.MediaType.ALBUM);
+        album1.setYear(1990);
 
-		MediaFile disc6track1 = new MediaFile();
-		disc6track1.setMediaType(MediaFile.MediaType.MUSIC);
-		disc6track1.setPath("a");
-		disc6track1.setDiscNumber(6);
-		disc6track1.setTrackNumber(1);
+        MediaFile album2 = new MediaFile();
+        album2.setMediaType(MediaFile.MediaType.ALBUM);
+        album2.setYear(1991);
 
-		MediaFile disc6track2 = new MediaFile();
-		disc6track2.setMediaType(MediaFile.MediaType.MUSIC);
-		disc6track2.setPath("a");
-		disc6track2.setDiscNumber(6);
-		disc6track2.setTrackNumber(2);
+        return comparator_1.compare(album1, album2) != 0;
+    }
 
-		assertEquals(0, comparator.compare(discXtrack1, discXtrack1));
-		assertEquals(0, comparator.compare(disc5track1, disc5track1));
+    // execute path: 1-3-5-6-7-8-10-11-15
+    private boolean TC6() {
+        MediaFile album1 = new MediaFile();
+        album1.setMediaType(MediaFile.MediaType.ALBUM);
+        album1.setYear(1990);
+        album1.setPath("album1");
 
-		assertEquals(-1, comparator.compare(discXtrack1, discXtrack2));
-		assertEquals(1, comparator.compare(discXtrack2, discXtrack1));
+        MediaFile album2 = new MediaFile();
+        album2.setMediaType(MediaFile.MediaType.ALBUM);
+        album2.setYear(1990);
+        album2.setPath("album2");
 
-		assertEquals(-1, comparator.compare(disc5track1, disc5track2));
-		assertEquals(1, comparator.compare(disc6track2, disc5track1));
+        return comparator_1.compare(album1, album2) != 0;
+    }
 
-		assertEquals(-1, comparator.compare(disc5track1, disc6track1));
-		assertEquals(1, comparator.compare(disc6track1, disc5track1));
+    // execute path: 1-3-5-6-7-10-12-13-15
+    private boolean TC7() {
+        MediaFile album1 = new MediaFile();
+        album1.setMediaType(MediaFile.MediaType.MUSIC);
+        album1.setTrackNumber(1);
 
-		assertEquals(-1, comparator.compare(disc5track2, disc6track1));
-		assertEquals(1, comparator.compare(disc6track1, disc5track2));
+        MediaFile album2 = new MediaFile();
+        album2.setMediaType(MediaFile.MediaType.MUSIC);
+        album2.setTrackNumber(2);
 
-		assertEquals(-1, comparator.compare(discXtrack1, disc5track1));
-		assertEquals(1, comparator.compare(disc5track1, discXtrack1));
+        return comparator_2.compare(album1, album2) != 0;
+    }
 
-		assertEquals(-1, comparator.compare(discXtrack1, disc5track2));
-		assertEquals(1, comparator.compare(disc5track2, discXtrack1));
-	}
+    // execute path: 1-3-5-6-7-10-12-14-15
+    private boolean TC8() {
+        MediaFile album1 = new MediaFile();
+        album1.setMediaType(MediaFile.MediaType.MUSIC);
+        album1.setTrackNumber(1);
+        album1.setPath("album1");
+
+        MediaFile album2 = new MediaFile();
+        album2.setMediaType(MediaFile.MediaType.MUSIC);
+        album2.setTrackNumber(1);
+        album2.setPath("album2");
+
+        return comparator_2.compare(album1, album2) != 0;
+    }
+
+    // execute path: 1-3-5-6-7-10-11-15
+    private boolean TC9() {
+        MediaFile album1 = new MediaFile();
+        album1.setMediaType(MediaFile.MediaType.DIRECTORY);
+        album1.setPath("album1");
+
+        MediaFile album2 = new MediaFile();
+        album2.setMediaType(MediaFile.MediaType.DIRECTORY);
+        album2.setPath("album2");
+
+        return comparator_2.compare(album1, album2) != 0;
+    }
+
+    public void testCompareAlbums() {
+        assertTrue(TC1());
+        assertTrue(TC2());
+        assertTrue(TC3());
+        assertTrue(TC4());
+        assertTrue(TC5());
+        assertTrue(TC6());
+        assertTrue(TC7());
+        assertTrue(TC8());
+        assertTrue(TC9());
+    }
+
+    public static void main(String[] arg) {
+        new MediaFileComparatorTestCase().testCompareAlbums();
+    }
 }
