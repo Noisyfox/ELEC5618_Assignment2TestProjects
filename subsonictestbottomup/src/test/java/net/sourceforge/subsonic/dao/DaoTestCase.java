152bbf81eac178d2a2193fb8e3bd0b207f0b8aa9
Noisyfox <timemanager.rick@gmail.com>
2017-06-07 19:42:53 +1000

    Add test cases for TranscodingDao

--- subsonictestbottomup/src/test/java/net/sourceforge/subsonic/dao/DaoTestCase.java
+++ subsonictestbottomup/src/test/java/net/sourceforge/subsonic/dao/DaoTestCase.java
@@ -2,10 +2,12 @@ package net.sourceforge.subsonic.dao;
 
 import junit.framework.TestCase;
 import net.sourceforge.subsonic.domain.InternetRadio;
+import net.sourceforge.subsonic.domain.Transcoding;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.io.File;
 import java.util.Date;
+import java.util.List;
 
 public class DaoTestCase extends TestCase
 {
@@ -51,10 +53,12 @@ public class DaoTestCase extends TestCase
 
 	public void testInternetRadioDao()
 	{
+		getJdbcTemplate().execute( "delete from internet_radio" );
+
 		InternetRadioDao internetRadioDao = new InternetRadioDao();
 		internetRadioDao.setDaoHelper( daoHelper );
 
-		// Test InternetRadioDao.createInternetRadio()
+		// Test InternetRadioDao.createInternetRadio() && InternetRadioDao.getAllInternetRadios()
 		InternetRadio radio = new InternetRadio( "name", "streamUrl", "homePageUrl", true, new Date() );
 		internetRadioDao.createInternetRadio( radio );
 		InternetRadio newRadio = internetRadioDao.getAllInternetRadios().get( 0 );
@@ -83,5 +87,62 @@ public class DaoTestCase extends TestCase
 		assertEquals( "Wrong home page url.", expected.getHomepageUrl(), actual.getHomepageUrl() );
 		assertEquals( "Wrong enabled state.", expected.isEnabled(), actual.isEnabled() );
 		assertEquals( "Wrong changed date.", expected.getChanged(), actual.getChanged() );
+	}
+
+	public void testTranscodingDao()
+	{
+		getJdbcTemplate().execute( "delete from transcoding2" );
+		getJdbcTemplate().execute( "delete from player_transcoding2" );
+		getJdbcTemplate().execute( "delete from player" );
+
+		TranscodingDao transcodingDao = new TranscodingDao();
+		transcodingDao.setDaoHelper( daoHelper );
+
+		// Test TranscodingDao.createTranscoding() and TranscodingDao.getAllTranscodings()
+		Transcoding transcoding = new Transcoding( null, "name",
+				"sourceFormats", "targetFormat",
+				"step1", "step2", "step3", false );
+		transcodingDao.createTranscoding( transcoding );
+		Transcoding newTranscoding = transcodingDao.getAllTranscodings().get( 0 );
+		assertTranscodingEquals( transcoding, newTranscoding );
+
+		// Test TranscodingDao.updateTranscoding()
+		transcoding = newTranscoding;
+		transcoding.setName( "newName" );
+		transcoding.setSourceFormats( "newSourceFormats" );
+		transcoding.setTargetFormat( "newTargetFormat" );
+		transcoding.setStep1( "newStep1" );
+		transcoding.setStep2( "newStep2" );
+		transcoding.setStep3( "newStep3" );
+		transcoding.setDefaultActive( true );
+		transcodingDao.updateTranscoding( transcoding );
+		newTranscoding = transcodingDao.getAllTranscodings().get( 0 );
+		assertTranscodingEquals( transcoding, newTranscoding );
+
+		// Test TranscodingDao.setTranscodingsForPlayer() & TranscodingDao.getTranscodingsForPlayer()
+		// Insert a test player
+		getJdbcTemplate().execute( "insert into player (id, name, type, username, ip_address, auto_control_enabled, " +
+				"last_seen, cover_art_scheme, transcode_scheme, dynamic_ip, technology, client_id) " +
+				"values (1, 'name', 'type', 'username', 'ip', true, 0, 0, 0, false, 0, 'android')" );
+		transcoding = newTranscoding;
+		transcodingDao.setTranscodingsForPlayer( "1", new int[]{transcoding.getId()} );
+		List<Transcoding> transcodings = transcodingDao.getTranscodingsForPlayer( "1" );
+		assertEquals( transcodings.size(), 1 );
+		assertTranscodingEquals( transcoding, transcodings.get( 0 ) );
+
+		// Test TranscodingDao.deleteTranscoding()
+		transcodingDao.deleteTranscoding( transcoding.getId() );
+		assertEquals( transcodingDao.getAllTranscodings().size(), 0 );
+	}
+
+	private void assertTranscodingEquals( Transcoding expected, Transcoding actual )
+	{
+		assertEquals( "Wrong name.", expected.getName(), actual.getName() );
+		assertEquals( "Wrong source formats.", expected.getSourceFormats(), actual.getSourceFormats() );
+		assertEquals( "Wrong target format.", expected.getTargetFormat(), actual.getTargetFormat() );
+		assertEquals( "Wrong step 1.", expected.getStep1(), actual.getStep1() );
+		assertEquals( "Wrong step 2.", expected.getStep2(), actual.getStep2() );
+		assertEquals( "Wrong step 3.", expected.getStep3(), actual.getStep3() );
+		assertEquals( "Wrong default active.", expected.isDefaultActive(), actual.isDefaultActive() );
 	}
 }
